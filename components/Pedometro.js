4491b638b41c396e8d6cdf4a215011c357b72131
SaraAyumi19 <saraayumimatsumoto@gmail.com>
2026-08-13 11:32:38 -0300

    Finalização da tela de Pedômetro

--- components/Pedometro.js
+++ components/Pedometro.js
@@ -11,15 +11,66 @@ export default function Pedometro() {
   useEffect(() => {
     let inscricao = null;
 
-    async function consiguraPedometro() {
-      
+    async function configurarPedometro() {
+      const estaDisponivel = await Pedometer.isAvailableAsync();
+      setDispponivel(estaDisponivel);
+
+      const inicio = new Date();
+      const fim = new Date();
+      inicio.setDate(fim.getDate() - 1);
+
+      let resultado;
+      if (Plataforma.OS == "ios") {
+        resultado = await Pedometer.getStepCountAsync(inicio, fim);
+
+        setPassosPassados(resultado.steps);
+
+        inscricao = Pedometer.watchStepCount((monitor) => {
+          setPassosAtuais(monitor.steps);
+        });
+      }
+      else {
+        const autorizado = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACTIVITY_RECOGNITION
+        );
+
+        if (autorizado === PermissionsAndroid.RESULTS.GRANTED){
+          inscricao = Pedometer.watchStepCount((monitor) => {
+            setPassosAtuais(monitor.steps);
+          })
+        }
+
+      }
     }
+
+    configurarPedometro();
+
   }, []);
+
+  function passosNoDia() {
+    if (Platform.OS == "ios") {
+      return <Text>Passos Hoje: {passosPassados}</Text>
+    }
+    else {
+      <Text>Sem Contagem Anterior Disponível</Text>
+    }
+  }
 
   
   return(
     <View>
-      <Text>Tela do Pedômetro</Text>
+      <Text>Contador de Passos</Text>
+      <View>
+        <Text>Sensor Disponível: 
+          { disponivel === true ? "Sim" : "Não"}
+        </Text>
+
+        {passosNoDia()}
+
+        <Text>Passos Atuais: 
+          {passosAtuais}
+        </Text>
+      </View>
     </View>
   );
 }
